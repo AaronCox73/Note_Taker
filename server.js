be5683feafa755d0781e42b6e89cc97b6c88087c
Aaron Cox <aaroncox@Aarons-iMac.local>
2021-10-09 18:39:07 -0600

    progress

--- server.js
+++ server.js
@@ -1,8 +1,59 @@
 const express = require('express');
-
+const PORT = process.env.PORT || 3001;
 const app = express();
+var fs = require('fs');
+const path = require("path");
 
 
-app.listen(3001, () => {
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+// get for "GET /notes should return the notes.html file."
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+// get for "GET * should return the index.html file."
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+// get for "GET /api/notes should read the db.json file and return all saved notes as JSON."
+app.get('/api/notes', (res, req) => {
+    let results = db;
+    console.log(req.query);
+    res.json(results);
+})
+
+
+// make an app.post to "WHEN I enter a new note title and the note’s text"
+app.post('/notes', (req, res) => {
+    //console.log(req.body.id);
+    req.body = note.length.toString();
+
+
+    res.json(db)
+});
+
+
+
+
+
+// make a delete request (as a bonus but not required by assignment)
+// app.delete('/notes/:id', req, res => {
+//     const id = req.params.id;
+
+//     Note.findByIdAndDelete(id)
+//         .then(result => {
+//             res.json({ redirect: '/notes' })
+//         })
+// });
+
+app.listen(PORT, () => {
     console.log(`API server now on port 3001!`)
 });
